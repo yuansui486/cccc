f9929d0213f499125c276025ad1bb304e469b194
yuansui486 <zxc872202364@163.com>
2026-07-02 14:04:55 +0800

    Revert "docs: add group automation api"
    This reverts commit 417580a4daee50a353590c538b16dcd3b3a795bd.

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -89,7 +89,6 @@ export default defineConfig({
             { text: 'Positioning', link: '/reference/positioning' },
             { text: 'Architecture', link: '/reference/architecture' },
             { text: 'Features', link: '/reference/features' },
-            { text: 'Group Automation API', link: '/reference/group-automation-api' },
             { text: 'CLI', link: '/reference/cli' }
           ]
         }
